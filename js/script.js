3177c337dbbcd675e4e82dde6b1f36df6c09928c
Shahetul Chisty <shahetulchisty@Shahetuls-MacBook-Pro.local>
2021-07-29 13:18:01 -0700

    Checkbox schedule conflict works, credit card validator, name and email validator

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const designOptionTag = designDropDown.getElementsByTagName("OPTION");
 const shirtColorsDropDown = document.getElementById("shirt-colors");
 const shirtColorOptionTag = shirtColorsDropDown.getElementsByTagName("OPTION");
 const dropDownOptions = document.querySelectorAll("#color option");
+const submitButton = document.querySelector("form");
+const activities = document.querySelector("#activities-box");
+let activitiesTotal = 0;
 
 nameField.focus();
 console.log(designDropDown[1]);
@@ -48,9 +51,6 @@ designDropDown.addEventListener("change", e =>{
     }    
 });
 
-
-console.log(dropDownOptions);
-
 designDropDown.addEventListener("change", e =>{
 
 for(let i = 0; i < dropDownOptions.length; i++){
@@ -63,18 +63,187 @@ for(let i = 0; i < dropDownOptions.length; i++){
 }
 });
 
-const registerActivities = document.querySelector("#activities");
 
+// Focused checkbox highlighted for better accessibility
+const registerActivities = document.querySelector("#activities");
+const checkbox = document.querySelectorAll("#activities input[type='checkbox']");
+
+for(let i = 0; i < checkbox.length; i++){
+    checkbox[i].onfocus = ()=>{
+        checkbox[i].parentNode.classList.add("focus");
+    }
+    checkbox[i].onblur = ()=>{
+        checkbox[i].parentNode.classList.remove("focus");
+    }
+}
 
 let total = 0;
 registerActivities.addEventListener("change", e =>{
-    const allActivities = document.querySelectorAll("#activities input");
     const activitiesCost = document.querySelector("#activities-cost");
+
     if(e.target.checked === true){
         total += parseInt(e.target.dataset.cost);
+        e.target.checked
     }else{
         total -= parseInt(e.target.dataset.cost);
     }
     total.toString();
     activitiesCost.textContent = `Total: $${total}`;
 });
+
+const paymentDropDown = document.querySelector("#payment");
+const paymentSelector = document.querySelectorAll("#payment option");
+
+paymentSelector[1].selected = true;
+
+const creditCard = document.querySelector("#credit-card");
+const paypal = document.querySelector("#paypal");
+const bitcoin = document.querySelector("#bitcoin");
+
+// hide all payment selctors
+paypal.style.display = "none";
+bitcoin.style.display = "none";
+
+// event listener change on dropdown
+paymentDropDown.addEventListener("change", e =>{
+    for(let i = 0; i < paymentSelector.length; i++){
+        if(e.target.value === "credit-card"){
+            creditCard.style.display = "block";
+            paypal.style.display = "none";
+            bitcoin.style.display = "none";
+        }else if(e.target.value === "paypal"){
+            paypal.style.display = "block";
+            creditCard.style.display = "none";
+            bitcoin.style.display = "none";
+        }else{
+            bitcoin.style.display = "block";
+            paypal.style.display = "none";
+            creditCard.style.display = "none";
+        }
+    }
+});
+
+// Check if activity checkboxes conflict with schedule times
+registerActivities.addEventListener("change", e =>{
+
+    for(let i = 0; i < checkbox.length; i++){
+
+        if(e.target.checked === true){
+            if(checkbox[i].disabled === false && checkbox[i] !== e.target){
+                if(checkbox[i].parentElement.children[2].textContent === e.target.parentElement.children[2].textContent){
+                    checkbox[i].disabled = true;
+                }
+            }
+        }
+
+        if(e.target.checked === false){
+            if(checkbox[i].disabled === true && checkbox[i] !== e.target){
+                if(checkbox[i].parentElement.children[2].textContent === e.target.parentElement.children[2].textContent){
+                    checkbox[i].disabled = false;
+                }
+            }
+        }
+
+    }
+});
+
+const validationPass = element =>{
+    element.parentElement.classList.add("valid");
+    element.parentElement.classList.remove("not-valid");
+    element.parentElement.lastElementChild.style.display = "none";
+}
+const validationFail = element =>{
+    element.parentElement.classList.add("not-valid");
+    element.parentElement.classList.remove("valid");
+    element.parentElement.lastElementChild.style.display = "block";
+}
+
+const nameValidator = ()=>{
+    const nameInputBox = document.querySelector("#name");
+        let nameValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]+ ?$/.test(nameInputBox.value);
+        console.log(`Name validator evaluates to be ${nameValid}`);
+        if(nameValid === true){
+            validationPass(nameInputBox);
+        }else{
+            validationFail(nameInputBox);
+        }
+    return nameValid;
+}
+
+const emailValidator = ()=>{
+    const email = document.querySelector("#email");
+    const emailIsValid = /^[^@]+@+[^@]+\.(com|net|org)+\s*$/i.test(email.value);
+    console.log(`Email validator evaluates to be ${emailIsValid}`);
+    if(emailIsValid === true){
+        validationPass(email);
+    }else{
+        validationFail(email);
+    }
+    return emailIsValid;
+}
+activities.addEventListener("change", e =>{
+    if(e.target.checked){
+        activitiesTotal++;
+    }else{
+        activitiesTotal--;
+    }
+    console.log(activitiesTotal);
+});
+
+const checkboxValidator = ()=>{
+    const isBoxChecked = activitiesTotal > 0;
+    console.log(`Box checked evaluates to be ${isBoxChecked}`);
+    if(isBoxChecked > 0){
+        validationPass(activities);
+    }else{
+        validationFail(activities);
+    }
+    return isBoxChecked;
+}
+const cardNumberValidator = ()=>{
+    const creditCardNumber = document.querySelector("#cc-num");
+    const validNumber = /^\d{13,16}\s*$/.test(creditCardNumber.value); 
+    console.log(`Card number is ${validNumber}`);
+    if(validNumber === true){
+        validationPass(creditCardNumber);
+    }else{
+        validationFail(creditCardNumber);
+    }
+    return validNumber;
+}
+const zipValidator = ()=>{
+    const zipcode = document.querySelector("#zip");
+    const validZip = /^\d{5}\s*$/.test(zipcode.value);
+    console.log(`Zip code is ${validZip}`);
+    if(validZip === true){
+        validationPass(zipcode);
+    }else{
+        validationFail(zipcode);
+    }
+    return validZip;
+}
+const securityCodeValidator = ()=>{
+    const cvv = document.querySelector("#cvv");
+    const validSecurityCode = /^\d{3,4}\s*$/.test(cvv.value);
+    console.log(`The security code validator evaluates to be ${validSecurityCode}`);
+    if(validSecurityCode === true){
+        validationPass(cvv);
+    }else{
+        validationFail(cvv);
+    }
+    return validSecurityCode;
+}
+
+submitButton.addEventListener("submit", e =>{
+    e.preventDefault();
+    nameValidator();
+    emailValidator();
+    checkboxValidator();
+    if(paymentDropDown.value === "credit-card"){
+        cardNumberValidator();
+        zipValidator();
+        securityCodeValidator();
+    }
+});
+
+// use prevent default if error occurs
